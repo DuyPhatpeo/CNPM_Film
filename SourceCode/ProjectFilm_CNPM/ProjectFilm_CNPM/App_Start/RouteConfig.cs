03f4b73883ea12f2e63a5e36e3e8d96f5d5c0013
Tran Duy Phat <115158729+DuyPhatpeo@users.noreply.github.com>
2024-05-16 16:35:10 +0700

    Thay đổi
    thay đổi nhiều cái

--- SourceCode/ProjectFilm_CNPM/ProjectFilm_CNPM/App_Start/RouteConfig.cs
+++ SourceCode/ProjectFilm_CNPM/ProjectFilm_CNPM/App_Start/RouteConfig.cs
@@ -16,7 +16,9 @@ namespace ProjectFilm_CNPM
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "ProjectFilm_CNPM.Controllers" }
+
             );
         }
     }
