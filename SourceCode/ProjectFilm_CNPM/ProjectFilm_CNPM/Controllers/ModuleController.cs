4da00d7c14d4cc7ab2e7036e9033501b03c4ec1d
luvuphuc <145564516+luvuphuc@users.noreply.github.com>
2024-05-19 16:37:29 +0700

    Slider

---
+++ SourceCode/ProjectFilm_CNPM/ProjectFilm_CNPM/Controllers/ModuleController.cs
@@ -0,0 +1,33 @@
+﻿using ProjectFilm_CNPM.Models;
+using ProjectFilm_CNPM.Models.ERD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectFilm_CNPM.Controllers
+{
+    public class ModuleController : Controller
+    {
+        // GET: Module
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult MainMenuSub()
+        {
+            var menus = db.Menus.Where(x => x.TrangThai ==1 && x.ViTri  == "MainMenu").ToList();
+            return PartialView("MainMenuSub", menus);
+           
+        }
+        public PartialViewResult Slider()
+        {
+            
+            List<Slider> list = db.Sliders.Where(m => m.ViTri == "Slider" && m.TrangThai == 1).ToList();
+            return PartialView("Slider", list);
+
+        }
+    }
+}
