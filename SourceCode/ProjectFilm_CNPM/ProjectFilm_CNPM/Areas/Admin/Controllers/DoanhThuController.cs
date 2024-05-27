6abfd6c3b5c7c09f7e2096ffd9a360dc8e30fc2c
luvuphuc <145564516+luvuphuc@users.noreply.github.com>
2024-05-27 16:21:10 +0700

    DONEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

--- SourceCode/ProjectFilm_CNPM/ProjectFilm_CNPM/Areas/Admin/Controllers/DoanhThuController.cs
+++ SourceCode/ProjectFilm_CNPM/ProjectFilm_CNPM/Areas/Admin/Controllers/DoanhThuController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ProjectFilm_CNPM.Models;
+using ProjectFilm_CNPM.Models.ERD;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,24 @@ namespace ProjectFilm_CNPM.Areas.Admin.Controllers
     public class DoanhThuController : Controller
     {
         // GET: Admin/DoanhThu
+        private readonly ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
-            return View();
+            var totalRevenuePerMovie = db.ChiTietHoaDons
+                .Where(cthd => cthd.HoaDon.TrangThai == 1) 
+                .Join(db.SuatChieus, cthd => cthd.MaSuatChieu, sc => sc.MaSuatChieu, (cthd, sc) => new { cthd, sc }) 
+                .GroupBy(x => x.sc.MaPhim)
+                .Select(g => new DoanhThuViewModel
+                {
+                    MaPhim = g.Key,
+                    TenPhim = g.FirstOrDefault().sc.Phim.TenPhim,
+                    NgayChieu = g.FirstOrDefault().sc.GioChieu,
+                    TongSoVe = g.Select(x => x.cthd).Count(),
+                    TongTien = g.Sum(x => x.cthd.HoaDon.TongTien) 
+                })
+                .ToList();
+
+            return View(totalRevenuePerMovie);
         }
     }
 }
---
+++ SourceCode/ProjectFilm_CNPM/ProjectFilm_CNPM/Models/ERD/DoanhThuViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectFilm_CNPM.Models.ERD
+{
+    public class DoanhThuViewModel
+    {
+        public int MaPhim { get; set; }
+        public string TenPhim { get; set; }
+        public int TongTien { get; set; }
+        public DateTime NgayChieu { get; set; }
+        public int TongSoVe {  get; set; }
+    }
+}
