c9f750eea356ea70a14a4f51e8a78f2396d8deb6
luvuphuc <145564516+luvuphuc@users.noreply.github.com>
2024-05-28 10:52:07 +0700

    edit(2)

---
+++ SourceCode/ProjectFilm_CNPM/ProjectFilm_CNPM/Controllers/TimKiemController.cs
@@ -0,0 +1,25 @@
+﻿using ProjectFilm_CNPM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectFilm_CNPM.Controllers
+{
+    public class TimKiemController : Controller
+    {
+        // GET: TimKiem
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index(string searchString)
+        {
+            ViewBag.searchString = searchString;
+            var phim = db.Phims.Where(p => p.TenPhim.ToLower().Contains(searchString.ToLower())).ToList();
+            if (phim != null)
+            {
+                return View(phim);
+            }
+            return View();
+        }
+    }
+}
